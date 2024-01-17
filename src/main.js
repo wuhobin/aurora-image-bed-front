a97ab41f95b5c26dc273d813fb53fe8aa4aa3d7d
wuhobin <wuhongbin@365huaer.net>
2024-01-17 11:15:35 +0800

    添加vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import api from "./api";
 import router from "@/router";
+import store from "@/store/index";
 
 Vue.config.productionTip = false
 
@@ -10,4 +11,5 @@ Vue.prototype.$http = api;
 new Vue({
   render: h => h(App),
   router,
+  store
 }).$mount('#app')
